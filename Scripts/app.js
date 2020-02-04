23f13ae4a5d3d52bee04a0dd6b0bc72ce42bcd1d
Spence McComb <spencemccomb@hotmail.com>
2020-02-03 19:52:50 -0500

    Added 5 pages to the site

--- Scripts/app.js
+++ Scripts/app.js
@@ -14,20 +14,54 @@ let app;
      */
     function Start()
     {
-      let mainContent = $("#mainContent")
-      console.log(mainContent);
-
-      let mainCont = document.getElementById("#mainContent");
-      console.log(mainCont);
-
-      let mainQuery = document.querySelectorAll("#mainContent")[0];
-      console.log(mainQuery);
-
-      let mainFirstQuery = document.querySelector("#mainContent");
-      console.log(mainQuery);
-
-      
+      PageSwitcher();
       Main();
+    }
+
+    function PageSwitcher() 
+    {
+      let name = window.location.pathname;
+      let pageName = name.substring(1, name.length - 5);
+      
+      switch(pageName) 
+      {
+        case "index":
+          HomePageContent();
+          break;
+        case "products":
+          break;
+        case "services":
+          break;
+        case "about":
+          break;
+        case "contact":
+          break;
+        case "projects":
+          break;
+        default:
+          console.error("Landed on a page that is undefined.")
+          break;
+      }
+
+      // Add a class of active to the current link (other than home, since pageName == index)
+      $("#"+pageName).addClass("active");
+
+      // Same thing in vanilla javascript
+      //document.querySelector("#"+pageName).className = "nav-item active";
+
+      //$(".btn.btn-primary").hide();
+
+      // Same thing, but JS gives an error on the other pages where the class doesn't exist
+      //document.getElementsByClassName("btn btn-primary")[0].style.display = "none";
+
+    }
+
+    function HomePageContent()
+    {
+      document.getElementById("home").className = "nav-item active";
+      $("button").click(()=>{
+        location.href = "projects.html";
+      })
     }
 
     /**
